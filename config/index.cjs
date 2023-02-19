734fe678adea4b379890856050c330d7153ebdfa
WangY111 <114199217+WangY111@users.noreply.github.com>
2023-02-19 19:11:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oVEtV6HjKbQy0pD3DFHYPkqYPvjc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'r-LNR-vYrXOysn8iOJJtYpKFuirzZRa7lTpAvgWqRPc',
+      useTemplateId: '	2ZZGzVXH13Hy9Eu0oJm0iHbF_BLIDRPhdd4LOcpBYtM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-17',
       festivals: [
