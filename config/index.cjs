99f6bd700952d1848c51b46b8f0e6750e0bfacf4
WangY111 <114199217+WangY111@users.noreply.github.com>
2023-02-19 19:18:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -49,7 +49,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	2ZZGzVXH13Hy9Eu0oJm0iHbF_BLIDRPhdd4LOcpBYtM',
+  CALLBACK_TEMPLATE_ID: '2ZZGzVXH13Hy9Eu0oJm0iHbF_BLIDRPhdd4LOcpBYtM',
 
   CALLBACK_USERS: [
     {
