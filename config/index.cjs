e3b5c5d13c4bd31f7cf8b80c0cfe9b226ea4258a
WangY111 <114199217+WangY111@users.noreply.github.com>
2023-02-19 18:28:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,17 +13,17 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '山东',
+  CITY: '济宁',
 
   USERS: [
     {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oVEtV6HjKbQy0pD3DFHYPkqYPvjc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'cP9LrYnjLiIdoY109JfnK60u3XGRI2R2A96rjcgrdTQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'cP9LrYnjLiIdoY109JfnK60u3XGRI2R2A96rjcgrdTQ',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oVEtV6E_GhDi2fhxLtyqSd8NoI2s',
     }
   ],
 
